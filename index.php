76eed50765322e8427314bb1ced400754f0cb450
jaycob14 <54973944+jaycob14@users.noreply.github.com>
2023-03-02 17:29:07 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!-- HEADER -->
+<?php
+$title = 'index';
+ require_once 'includes/header.php';
+ ?>
+
+    
+        <h1>Hello World</h1>
+    
+
+<!-- FOOTER -->
+<?php require_once 'includes/footer.php';?>
+
